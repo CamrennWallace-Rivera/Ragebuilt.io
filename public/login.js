6bb579edc007728b89cf95b7c11ad9b9626b68ad
stevenLeecode <lees28@southernct.edu>
2025-03-26 06:21:29 -0400

    adding session storage

--- public/login.js
+++ public/login.js
@@ -7,6 +7,7 @@ signInBtn.addEventListener("click", (e) => {
 	var email = document.getElementById("email");
 	var pw = document.getElementById("pw");
 
+
 	let AJAX = new XMLHttpRequest();
 
         AJAX.onerror = function(e) {
@@ -15,11 +16,16 @@ signInBtn.addEventListener("click", (e) => {
 
         AJAX.onload = function() {
                 if (this.status == 200) {
-                        console.log("Sucessfully loaded, below is responseText.")
-                        console.log(this.responseText);
+                        if (this.responseText == "Invalid Login") {
+                                invalidLogin(pw);
+                        }
+                        else{
+                                sessionStorage.setItem("isLoggedin", true);
+                                let json = JSON.parse(this.responseText);
+                                console.log("Returned json: ", json)
+                        }
                 }
                 else{
-                        alert(this.status);
                         console.log("error");
         }}
         AJAX.open("GET", url + "login?email="+email.value+"&password="+pw.value);
@@ -27,3 +33,17 @@ signInBtn.addEventListener("click", (e) => {
         AJAX.send();
 
 });
+
+function invalidLogin(pw) {
+        const pwDiv = pw.closest("div");
+
+        let existingError = document.getElementById("login-error");
+        if (existingError) {
+                existingError.remove();
+        }
+        const errorDiv = document.createElement("div");
+        errorDiv.id = "login-error";
+        errorDiv.className = "flex items-center justify-center mt-2";
+        errorDiv.innerHTML = `<p class="text-red-800 font-bold"> Invalid Credentials, try again. </p>`;
+        pwDiv.parentNode.insertBefore(errorDiv, pwDiv.nextSibling);
+};
