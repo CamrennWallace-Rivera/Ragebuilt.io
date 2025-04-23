0550ceb58e9001ebf5ec2385704a165875fc04ba
stevenLeecode <lees28@southernct.edu>
2025-04-23 00:01:20 +0000

    Changed Vehicle Post Layout

--- public/UserVehiclePost.js
+++ public/UserVehiclePost.js
@@ -25,16 +25,28 @@ AJAX.onload = function() {
 
 		document.getElementById("vb-name").textContent = vb_data[0].vb_name || "No Vehicle Name";
         	document.getElementById("vb-price").textContent = vb_data[0].vb_price ? `$${vb_data[0].vb_price}` : "$0";
-        	document.getElementById("vb-engine-name").innerHTML = `<strong>Engine:</strong> ${vb_data[0].engine_name}`;
-		document.getElementById("vb-transmission-name").innerHTML = `<strong>Transmission:</strong> ${vb_data[0].transmission_name}`;
-		document.getElementById("vb-clutch-name").innerHTML = `<strong>Clutch:</strong> ${vb_data[0].clutch_name}`;
+        	document.getElementById("vb-engine-name").innerHTML = `${vb_data[0].engine_name}`;
+		document.getElementById("vb-engine-price").innerHTML = "$" + vb_data[0].engine_price;
+		document.getElementById("vb-engine-from").innerHTML = vb_data[0].engine_store;
+		document.getElementById("vb-transmission-name").innerHTML = `${vb_data[0].transmission_name}`;
+		document.getElementById("vb-transmission-price").innerHTML = "$" + vb_data[0].transmission_price;
+		document.getElementById("vb-transmission-from").innerHTML = vb_data[0].transmission_store;
+		document.getElementById("vb-clutch-name").innerHTML = `${vb_data[0].clutch_name}` || "N/A";
+		document.getElementById("vb-clutch-price").innerHTML = "$" + vb_data[0].clutch_price;
+		document.getElementById("vb-clutch-from").innerHTML = vb_data[0].clutch_store;
 		document.getElementById("vb-comments").textContent = vb_data[0].owner_comments || "No comments provided.";
 		document.getElementById("vb-specs").textContent = vb_data[0].additional_specs || "No additional specifications provided.";
 		document.getElementById("vb-username").textContent = "@" + vb_data[0].username;
 		document.getElementById("vb_picture").src = vb_data[0].vb_picture || "default_pfp.jpg";
-		document.getElementById("vb-tires-name").innerHTML =`<strong> Tires: </strong> ${vb_data[0].tire_name}`;
-		document.getElementById("vb-brakes-name").innerHTML = `<strong> Brakes: </strong> ${vb_data[0].brake_name}`;
-		document.getElementById("vb-muffler-name").innerHTML = `<strong> Muffler: </strong> ${vb_data[0].muffler_name}`;
+		document.getElementById("vb-tires-name").innerHTML =`${vb_data[0].tire_name}`;
+		document.getElementById("vb-tires-price").innerHTML = "$" + vb_data[0].tire_price;
+		document.getElementById("vb-tires-from").innerHTML = vb_data[0].tire_store;
+		document.getElementById("vb-brakes-name").innerHTML = `${vb_data[0].brake_name}`;
+		document.getElementById("vb-brakes-price").innerHTML = `$${vb_data[0].brake_price}`;
+		document.getElementById("vb-brakes-from").innerHTML = vb_data[0].brake_store 
+		document.getElementById("vb-muffler-name").innerHTML = `${vb_data[0].muffler_name}`;
+		document.getElementById("vb-muffler-price").innerHTML = `$${vb_data[0].muffler_price}`;
+		document.getElementById("vb-muffler-from").innerHTML = `${vb_data[0].muffler_store}`;
 		document.getElementById("vb_picture").classList.add("h-100");
 
 		const vb_email = vb_data[0].email;
